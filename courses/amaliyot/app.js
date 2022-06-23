399caeb03483ab5b5b292c3ff70e8ee1158f5a97
Muhammad <Muhammad>
2022-06-23 09:30:09 +0500

    first commit

--- courses/amaliyot/app.js
+++ courses/amaliyot/app.js
@@ -10,14 +10,32 @@ let seriesDB = {
   privat:false
 }
 
-let a = prompt("oxirgi ko'rgan serialingiz",""),
-b = +prompt('nechi baxo berasiz',""),
-c = prompt("oxirgi ko'rgan serialingiz","");
 
-let d = +prompt('nechi baxo berasiz',"");
+for(let i = 0;i<2;i++){
+  let a = prompt("oxirgi ko'rgan serialingiz",""),
+  b = +prompt('nechi baxo berasiz',"");
+  if(a!=null && b!==null && a != '' && b != ''){
+    console.log('done');
+    seriesDB.series[a] = b;
+  }else{
+    console.log('error');
+    i--;
+  }
+ 
+}
 
 
-seriesDB.series[a] = c
-seriesDB.series[c] = d
+if(seriesDB.count < 5){
+  console.log('siz kam serial ko`rgansiz');
+}else if(seriesDB.count >= 5 && seriesDB.count <10){
+  console.log('siz classic tomoshabinsiz');
+}else if(seriesDB.count>=10){
+  console.log('siz serialchi zvezdasiz');
+}
+else{
+  console.log('error');
+}
+
+
 
 console.log(seriesDB);
